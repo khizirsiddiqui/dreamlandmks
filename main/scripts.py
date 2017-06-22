192f5cc55fb9d54ae1c3f9c24fc44ab5fc8ac794
KhizirSiddiqui <khizirsiddiqui@gmail.com>
2017-06-22 15:23:03 +0530

    Encode quotes into UTF-8

--- main/scripts.py
+++ main/scripts.py
@@ -50,7 +50,7 @@ def get_news():
 
 def get_random_quote():
     file = open('main/quote_list.txt')
-    quote_list = file.readlines()
+    quote_list = file.readlines().encode('utf-8')
     total = len(quote_list) - 1
     quote_text = quote_list[randint(0, total)]
     return quote_text.split(' -')
